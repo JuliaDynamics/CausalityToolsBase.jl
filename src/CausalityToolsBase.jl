24e24c40a3205235f8b6d5cce626627584b75292
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-01-05 01:55:35 +0100

    CausalityToolsBase module

---
+++ src/CausalityToolsBase.jl
@@ -0,0 +1,11 @@
+module CausalityToolsBase
+	include("discretization.jl")
+	include("dimension_estimation.jl")
+end # module
+
+"""
+	CausalityToolsBase
+
+A lightweight module containing data structures and algorithms used throughout the 
+CausalityTools ecosystem.
+"""
