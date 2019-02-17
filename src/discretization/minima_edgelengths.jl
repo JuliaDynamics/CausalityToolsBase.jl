58ec2e063325a8a1f8c3701ebcc7550e85b24853
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-02-17 15:31:40 +0100

    Get minima and edgelengths when specifying min/max of the grid

--- src/discretization/minima_edgelengths.jl
+++ src/discretization/minima_edgelengths.jl
@@ -1,7 +1,59 @@
+import DelayEmbeddings: Dataset, minima, maxima
+import StaticArrays: SVector, MVector
+
 export 
 get_minima_and_edgelengths, 
 get_edgelengths, 
-get_minima
+get_minima,
+get_minmaxes
+
+
+"""
+    get_minima(pts) -> Vector{Float}
+
+Return the minima along each axis of the dataset `pts`.
+"""
+function get_minima end
+
+"""
+    get_maxima(pts) -> Vector{Float}
+
+Return the maxima along each axis of the dataset `pts`.
+"""
+function get_minima end
+
+function get_minima(pts::Dataset)
+    minima(pts)
+end
+
+function get_minima(pts::Vector{T}) where {T <: Union{SVector, MVector, Vector}}
+    minima(Dataset(pts))
+end
+
+function get_maxima(pts::Dataset)
+    maxima(pts)
+end
+
+function get_maxima(pts::Vector{T}) where {T <: Union{SVector, MVector, Vector}}
+    maxima(Dataset(pts))
+end
+
+"""
+    get_minmaxes(pts) -> Tuple{Vector{Float}, Vector{Float}}
+
+Return a vector of tuples containing axis-wise (minimum, maximum) values.
+"""
+function get_minmaxes end
+
+function get_minmaxes(pts::Dataset)
+    mini, maxi = minima(pts), maxima(pts)
+    minmaxes = [(mini[i], maxi[i]) for i = 1:length(mini)]
+end
+
+function get_minmaxes(pts::Vector{T}) where {T <: Union{SVector, MVector, Vector}}
+    get_minmaxes(Dataset(pts))
+end
+
 
 """
     get_minima_and_edgelengths(points, ϵ) -> (Vector{Float}, Vector{Float})
@@ -43,10 +95,10 @@ function get_minima_and_edgelengths(points, ϵ)
     elseif ϵ isa Tuple{Vector{Tuple{Float64, Float64}}, Int}
         # We have predefined axis minima and axis maxima.
         n_bins = ϵ[2]
-        stepsizes = zeros(Float64, dim)
-        edgelengths = zeros(Float64, dim)
+        stepsizes = zeros(Float64, D)
+        edgelengths = zeros(Float64, D)
 
-        for i = 1:dim 
+        for i = 1:D
             edgelengths[i] = (maximum(ϵ[1][i]) - minimum(ϵ[1][i]))/n_bins
             axisminima[i] = minimum(ϵ[1][i])
         end
@@ -55,9 +107,18 @@ function get_minima_and_edgelengths(points, ϵ)
     axisminima, edgelengths
 end
 
-get_minima(points, ϵ) = get_minima_and_edgelengths(points, ϵ)[1]
-get_edgelengths(points, ϵ) = get_minima_and_edgelengths(points, ϵ)[2]
-
 get_minima_and_edgelengths(points, ϵ::RectangularBinning) = get_minima_and_edgelengths(points, ϵ.ϵ)
-get_minima(points, ϵ::RectangularBinning) = get_minima(points, ϵ.ϵ)
-get_edgelengths(points, ϵ::RectangularBinning) = get_minima(points, ϵ.ϵ)
+
+
+"""
+    get_edgelengths(pts, ϵ) -> Vector{Float}
+
+Return the box edge length along each axis resulting from 
+discretizing `pts` on a rectangular grid specified by the 
+binning scheme `ϵ`.
+"""
+function get_edgelengths end
+
+get_edgelengths(points, ϵ) = get_minima_and_edgelengths(points, ϵ)[2]
+get_edgelengths(points, ϵ::RectangularBinning) = get_edgelengths(points, ϵ.ϵ)
+
