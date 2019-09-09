24018cfc796ea238e1b75d6cba29cbefbac3a741
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2019-09-09 14:16:09 +0200

    `CausalityTest` replaces `CausalityEstimatorTest` (#22)
    * Base.@kwdef used for default values in test types requires Julia >= 1.1
    
    That'll be a minor version increment.
    
    * Changelog
    
    * Revert "Revert "Rename `CausalityEstimatorParams` and related types to `CausalityTest`""
    
    This reverts commit fe387da46ecce5f34a42adf27d8b0a5966ade479.
    
    * `CausalityTest` replaces `CausalityEstimatorTest`

---
+++ src/causalitytest.jl
@@ -0,0 +1,25 @@
+
+"""
+CausalityTest
+
+An abstract type that is the supertype of all causality test types 
+in the `CausalityTools` ecosystem. 
+
+The naming convention for abstract subtypes is `SomeMethodTest`. Examples of
+the type hierarchy of abstract test types could be:
+
+- `TransferEntropyTest <: CausalityTest`
+- `CrossMappingTest <: CausalityTest`
+
+
+Subtypes of those abstract types are named according to the specific algorithm. Examples
+of complete type hierachies for specific causality test types could be:
+
+- `VisitationFrequencyTest <: TransferEntropyTest <: CausalityTest`.
+- `TransferOperatorGridTest <: TransferEntropyTest <: CausalityTest`.
+- `SimpleCrossMapTest <: CrossMappingTest <: CausalityTest`.
+- `ConvergentCrossMapTest <: CrossMappingTest <: CausalityTest`.
+"""
+abstract type CausalityTest end
+
+export CausalityTest
