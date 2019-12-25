d913e3c2d65ff78c8ae3f7a6a443ff2bdad31212
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2019-12-25 19:51:43 +0100

    Update causalitytest.jl

--- src/causalitytest.jl
+++ src/causalitytest.jl
@@ -1,9 +1,8 @@
 
 """
-CausalityTest
+    CausalityTest
 
-An abstract type that is the supertype of all causality test types 
-in the `CausalityTools` ecosystem. 
+An abstract type that is the supertype of all causality tests in the `CausalityTools` ecosystem. 
 
 The naming convention for abstract subtypes is `SomeMethodTest`. Examples of
 the type hierarchy of abstract test types could be:
@@ -11,14 +10,12 @@ the type hierarchy of abstract test types could be:
 - `TransferEntropyTest <: CausalityTest`
 - `CrossMappingTest <: CausalityTest`
 
-
 Subtypes of those abstract types are named according to the specific algorithm. Examples
 of complete type hierachies for specific causality test types could be:
 
 - `VisitationFrequencyTest <: TransferEntropyTest <: CausalityTest`.
 - `TransferOperatorGridTest <: TransferEntropyTest <: CausalityTest`.
-- `SimpleCrossMapTest <: CrossMappingTest <: CausalityTest`.
-- `ConvergentCrossMapTest <: CrossMappingTest <: CausalityTest`.
+- `CrossMappingTest <: DistanceBasedTest <: CausalityTest`.
 """
 abstract type CausalityTest end
 
