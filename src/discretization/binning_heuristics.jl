143eeb20155f2abba9a1a6968a57d34bfda20d32
Kristian Agasøster Haaga <kahaaga@gmail.com>
2020-01-30 19:59:51 +0100

    Put AbstractParameterOptimisation in a more logical place

---
+++ src/discretization/binning_heuristics.jl
@@ -0,0 +1,8 @@
+export BinningHeuristic
+
+"""
+    BinningHeuristic
+
+Abstract type for binning heuristics.
+"""
+abstract type BinningHeuristic  <: AbstractParameterOptimisation end
--- src/discretization/discretization.jl
+++ src/discretization/discretization.jl
@@ -1,5 +1,6 @@
 
 include("binning_schemes.jl")
+include("binning_heuristics.jl")
 include("bin_encode.jl")
 include("minima_edgelengths.jl")
 include("histograms_and_binvisits.jl")
